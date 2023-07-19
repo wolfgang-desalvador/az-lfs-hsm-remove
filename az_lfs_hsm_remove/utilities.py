a3bdc364d7ecd038fc355f0ab6b881f865fddbcc
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2023-07-19 11:00:32 +0200

    Fix check of fileStatus

--- az_lfs_hsm_remove/utilities.py
+++ az_lfs_hsm_remove/utilities.py
@@ -8,12 +8,12 @@ def loadConfiguration(file):
     return configuration
 
 def checkFileStatus(filePath):
-    fileStatus = subprocess.check_output(["lfs", "hsm_state", filePath])
+    fileStatus = str(subprocess.check_output(["lfs", "hsm_state", filePath]))
     logger = logging.getLogger()
     if 'released' in fileStatus:
         logger.error('The file is not restored to Lustre and cannot be removed from backend. Please release the file before.')
         return False
-    elif 'archieved' not in fileStatus:
+    elif 'archived' not in fileStatus:
         logger.error('The file does not appear to be in the backend.')
         return False
     else:
