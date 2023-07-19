f030ce02a946e1ea513f363502e1ba4926076537
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2023-07-19 21:15:47 +0200

    Add folder check

--- az_lfs_hsm_remove/main.py
+++ az_lfs_hsm_remove/main.py
@@ -6,10 +6,12 @@ from .lfs_blob_client import LFSBlobClient
 
 def main():
     fileToRemove = sys.argv[-1]
+    logger = logging.getLogger()
 
-    if os.path.exists(fileToRemove):
+    if os.path.isdir(fileToRemove):
+        logger.error('HSM operates on folders, not on files. The input path refers to a folder.')
+    elif os.path.exists(fileToRemove):
         LFSBlobClient().lfs_hsm_remove(fileToRemove)
     else:
-        logger = logging.getLogger()
         logger.error('The file provided does not exist on the system')
     
