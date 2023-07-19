cad00cf041fe8f8f05d2cb8b750b78fc0a1e5895
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2023-07-19 15:57:24 +0200

    Fix authentication warnings

--- az_lfs_hsm_remove/lfs_blob_client.py
+++ az_lfs_hsm_remove/lfs_blob_client.py
@@ -12,7 +12,7 @@ class LFSBlobClient(BlobServiceClient):
         configuration = loadConfiguration(configurationFile)
         self.accountURL = configuration.get('accountURL')
         self.containerName = configuration.get('containerName')
-        super().__init__(self.accountURL, credential=DefaultAzureCredential(exclude_workload_identity_credential=False, exclude_environment_credential=False), **kwargs)
+        super().__init__(self.accountURL, credential=DefaultAzureCredential(exclude_workload_identity_credential=True, exclude_environment_credential=True), **kwargs)
 
     def lfs_hsm_remove(self, filePath):
         absolutePath = os.path.abspath(filePath)
