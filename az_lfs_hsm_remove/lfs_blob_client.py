fee3c51b667c7ca823507c2c1daceacb3ae4575e
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2023-08-09 20:08:05 +0200

    Fix error handling

--- az_lfs_hsm_remove/lfs_blob_client.py
+++ az_lfs_hsm_remove/lfs_blob_client.py
@@ -1,8 +1,10 @@
 import os
 import subprocess
+import logging
 
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
+from azure.core.exceptions import ResourceNotFoundError
 
 from .utilities import loadConfiguration, checkFileStatus, get_relative_path
 
@@ -17,7 +19,14 @@ class LFSBlobClient(BlobServiceClient):
     def lfs_hsm_remove(self, filePath, force=False):
         absolutePath = os.path.abspath(filePath)
         if force or checkFileStatus(absolutePath):
-            client = self.get_blob_client(container=self.containerName, blob=get_relative_path(absolutePath))
-            client.delete_blob()
+            try:
+                client = self.get_blob_client(container=self.containerName, blob=get_relative_path(absolutePath))
+                client.delete_blob()
+            except ResourceNotFoundError:
+                logger = logging.getLogger()
+                if force:
+                    logger.info("Data seems not to be anymore on the HSM backend.")
+                else:
+                    logger.error("Data seems not to be anymore on the HSM backend even if hsm_state expects it to be there.")
             subprocess.check_output(["lfs", "hsm_set", "--lost", absolutePath])
             subprocess.check_output(["lfs", "hsm_set", "--dirty", absolutePath])
